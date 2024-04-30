26f3abb801a7b207e1f01d72b33b67d685084a47
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-04-30 12:44:20 +0200

    add

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,34 +1,60 @@
 import { Component } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 class AddComment extends Component {
   state = {
-    comment: "",
-    rate: "",
-    elementId: "",
+    commentUser: {
+      comment: "",
+      rate: "",
+      elementId: this.props.asin,
+    },
   };
   fetchAddComment = () => {
-    fetch(
-      "https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin,
-      {
-        method: "POST",
-        body: JSON.stringify(),
-        headers: {
-          ContentType: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZWNmNTdmMzA0NjAwMWFlNTlmNmYiLCJpYXQiOjE3MTQzOTUyMjgsImV4cCI6MTcxNTYwNDgyOH0.1rt6R-Dd1KgRS2MfszIvjSL3fZj2zqKl5zBz2LAt57Y",
-        },
-      }
-    );
+    fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+      method: "POST",
+      body: JSON.stringify(this.state.commentUser),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZWNmNTdmMzA0NjAwMWFlNTlmNmYiLCJpYXQiOjE3MTQzOTUyMjgsImV4cCI6MTcxNTYwNDgyOH0.1rt6R-Dd1KgRS2MfszIvjSL3fZj2zqKl5zBz2LAt57Y",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          this.setState({
+            commentUser: {
+              comment: "",
+              rate: "",
+              elementId: this.props.asin,
+            },
+          });
+        } else {
+          throw new Error("Errore nella fetch");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.fetchAddComment();
+  };
+  handleFieldChange = (propName, propValue) => {
+    this.setState({
+      commentUser: { ...this.state.commentUser, [propName]: propValue },
+    });
   };
 
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Select aria-label="Default select example">
+            <Form.Label>Lascia un commento</Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              value={this.state.commentUser.rate}
+              onChange={(e) => this.handleFieldChange("rate", e.target.value)}
+            >
               <option>Open this select menu</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -38,9 +64,17 @@ class AddComment extends Component {
             </Form.Select>
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Label>Scrivi il tuo commento</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={this.state.commentUser.comment}
+              onChange={(e) =>
+                this.handleFieldChange("comment", e.target.value)
+              }
+            />
           </Form.Group>
+          <Button type="submit">Invia Commento</Button>
         </Form>
       </div>
     );
